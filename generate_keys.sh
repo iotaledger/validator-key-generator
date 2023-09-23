8e83ade32be59e2ac8beadf5842c9e2243bcc9b9
Thomas Shufps <shufps80@gmail.com>
2023-09-23 09:43:06 +0200

    Update generate_keys.sh
    removed python3.10 assumption

--- generate_keys.sh
+++ generate_keys.sh
@@ -16,7 +16,7 @@ do
     [ -f "${file}" ] && error "won't overwrite $file"
 done
 
-# Check if Python 3.10 is installed
+# Check if Python 3 is installed
 test -x "$(which python3)" || error "Python 3 is not installed. Install it with:\n\tsudo apt update && sudo apt install python3"
 
 # Check if pip for Python 3 is installed
@@ -26,7 +26,7 @@ test -x "$(which pip3)" || error "pip3 is not installed. Install it with:\n\tsud
 test -x "$(which virtualenv)" || error "virtualenv is not installed. Install it with:\n\tsudo apt install python3-virtualenv"
 
 # Create virtual environment
-python3 -m virtualenv -p /usr/bin/python3.10 venv || error "Failed to create virtual environment"
+python3 -m virtualenv -p $( which python3 ) venv || error "Failed to create virtual environment"
 
 # Activate virtual environment
 source venv/bin/activate || error "Failed to activate virtual environment"
